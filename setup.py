bf06a265c83e5fc77c1fb17a988fe6c089b9ad63
Nivaeditha Suresh Sai <nss30@itl200.student.eecs.qmul.ac.uk>
2017-04-01 16:58:01 +0100

    week 10 full

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='YourAppName',
       author='Your Name',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask','flask-wtf','flask-babel','markdown','flup','wtforms','unirest','python-form','bokeh'],
+      install_requires=['Flask','flask-wtf','flask-babel','markdown','flup','wtforms','unirest','python-form','bokeh','six'],
       )
